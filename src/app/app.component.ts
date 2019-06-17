9a578e9d6a5f4bd40e25d002b3a003c8a4e340e1
Jason Hart <jasonhart179@gmail.com>
2019-06-17 13:30:30 -0500

    importing my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fib';
+
+  sum: number;
+  startPoint: number;
+  initialValue: number;
+
+  calculate(first: number) {
+    first = first - 2;
+    this.startPoint = 0;
+    this.initialValue = 1;
+
+    if (first > 0) {
+      do {
+        this.sum = this.startPoint + this.initialValue;
+        this.startPoint = this.initialValue;
+        this.initialValue = this.sum;
+        first = first - 1;
+      } while (first > 0)
+      return this.sum;
+    } else {
+      if (first == 1) {
+        this.sum = first;
+        return this.sum;
+      } else {
+        this.sum = 0;
+        return this.sum;
+      }
+    }
+
+
+  }
 }
